2ab34a17b764f76f8fb567557aabdc78071f322c
aaa18-txk02 <ashish.alex10@gmail.com>
2021-07-21 19:45:56 +0100

    Update test

--- src/test.py
+++ src/test.py
@@ -6,16 +6,29 @@ from torch.autograd import Variable
 import time
 from tqdm import trange
 
-inp_size = 6
-model = nn.Sequential(
-    nn.Linear(inp_size, inp_size),
-)
+
+class ProbModel(nn.Module):
+    def __init__(self, input_size=3):
+        super(ProbModel, self).__init__()
+        self.input_size = input_size
+        self.linear = nn.Linear(self.input_size, self.input_size)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        out = self.linear(x)
+        out = self.sigmoid(out)
+        return out
+
+input_size = 3
+model = ProbModel(input_size=input_size)
 opt = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-_input = torch.randn(1, inp_size)
+_input = torch.randn(1, input_size)
 print(f'_input:{_input}')
 # ground_truth = torch.randn(1, 3)
-ground_truth = _input
+ground_truth = torch.Tensor([0.5, 0.3, 0.2])
+print(f'ground_truth:{ground_truth}')
+# ground_truth = _input
 
 loss_function = nn.L1Loss()
 
@@ -29,12 +42,9 @@ for i in pbar:
     loss.backward()
     opt.step()
     pbar.set_postfix({'loss': loss.item(), 
-                      # 'pred1': output[0][0].item(), 
-                      # 'pred2': output[0][1].item(), 
-                      # 'pred3': output[0][2].item(),
-                      # 'pred4': output[0][3].item(),
-                      # 'pred5': output[0][4].item(),
-                      # 'pred6': output[0][5].item(),
+                      'pred1': output[0][0].item(), 
+                      'pred2': output[0][1].item(), 
+                      'pred3': output[0][2].item(),
                       })
     time.sleep(0.1)
 
